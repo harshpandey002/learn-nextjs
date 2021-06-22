344be473a42ed29e942dec327e84ffd6df9e67aa
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-22 19:13:36 +0530

    getStaticProps

--- pages/frost/index.js
+++ pages/frost/index.js
@@ -1,7 +1,28 @@
-const Frost = () => {
+import styles from "../../styles/Frost.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await res.json();
+
+  return {
+    props: {
+      frosts: data,
+    },
+  };
+};
+
+const Frost = ({ frosts }) => {
   return (
     <>
-      <h1>Index file of Frost Directory.</h1>
+      <h1>Frosts</h1>
+      {frosts.map((frost) => (
+        <div key={frost.id}>
+          <a className={styles.single}>
+            <h3>{frost.name}</h3>
+          </a>
+        </div>
+      ))}
     </>
   );
 };
