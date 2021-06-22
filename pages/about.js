b2a27a719b552d2677ea433d4607a9fd0d78e98c
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-22 15:24:08 +0530

    Routing

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About File in Pages Direcory</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vel,
+        inventore natus autem quasi esse fugit. Ipsam excepturi error quae dolor
+        repudiandae quisquam dicta accusantium perspiciatis placeat ratione.
+        Eius, quod!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vel,
+        inventore natus autem quasi esse fugit. Ipsam excepturi error quae dolor
+        repudiandae quisquam dicta accusantium perspiciatis placeat ratione.
+        Eius, quod!
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/frost/index.js
@@ -0,0 +1,9 @@
+const Frost = () => {
+  return (
+    <>
+      <h1>Index file of Frost Directory.</h1>
+    </>
+  );
+};
+
+export default Frost;
