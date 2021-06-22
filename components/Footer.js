833140a3df187647fddfc3ca3a506695efe84833
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-22 15:29:08 +0530

    Other Components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>Copyright 2021 Frost List</p>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Index file in Pages Directory</h1>
+      <Footer />
     </div>
   );
 }
