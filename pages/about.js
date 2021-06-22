7f14acf96fa68686751eb806d487143cbad28f9a
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-22 18:58:18 +0530

    Image & meta Tags

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About File in Pages Direcory</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vel,
-        inventore natus autem quasi esse fugit. Ipsam excepturi error quae dolor
-        repudiandae quisquam dicta accusantium perspiciatis placeat ratione.
-        Eius, quod!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vel,
-        inventore natus autem quasi esse fugit. Ipsam excepturi error quae dolor
-        repudiandae quisquam dicta accusantium perspiciatis placeat ratione.
-        Eius, quod!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Frost List | About</title>
+        <meta name="keywords" content="frost" />
+      </Head>
+      <div>
+        <h1>About File in Pages Direcory</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          vel, inventore natus autem quasi esse fugit. Ipsam excepturi error
+          quae dolor repudiandae quisquam dicta accusantium perspiciatis placeat
+          ratione. Eius, quod!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          vel, inventore natus autem quasi esse fugit. Ipsam excepturi error
+          quae dolor repudiandae quisquam dicta accusantium perspiciatis placeat
+          ratione. Eius, quod!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,29 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Index file in Pages Directory</h1>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
-        eligendi velit vel labore atque! Quidem reprehenderit ex autem ratione
-        inventore omnis saepe hic, ipsam laboriosam delectus voluptates corrupti
-        velit nesciunt!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
-        eligendi velit vel labore atque! Quidem reprehenderit ex autem ratione
-        inventore omnis saepe hic, ipsam laboriosam delectus voluptates corrupti
-        velit nesciunt!
-      </p>
-      <Link href="/frost">
-        <a className={styles.btn}>Frost Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Frost List | Home</title>
+        <meta name="keywords" content="frost" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Index file in Pages Directory</h1>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+          eligendi velit vel labore atque! Quidem reprehenderit ex autem ratione
+          inventore omnis saepe hic, ipsam laboriosam delectus voluptates
+          corrupti velit nesciunt!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+          eligendi velit vel labore atque! Quidem reprehenderit ex autem ratione
+          inventore omnis saepe hic, ipsam laboriosam delectus voluptates
+          corrupti velit nesciunt!
+        </p>
+        <Link href="/frost">
+          <a className={styles.btn}>Frost Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
